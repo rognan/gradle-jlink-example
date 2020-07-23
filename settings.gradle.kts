65a40b0d57dc4d69daf2d63302e3d3d6cdbc15ab
Thor Andreas Rognan <thor.rognan@gmail.com>
2020-07-23 02:29:13 +0200

    Upgrade ben-manes.versions plugin from v0.27.0 to v0.29.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
   plugins {
-    id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.github.ben-manes.versions") version "0.29.0"
     id("net.nemerosa.versioning") version "2.10.0"
   }
 }
