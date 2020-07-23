55eb6d3a98db5675ae49339b8808627d73790050
Thor Andreas Rognan <thor.rognan@gmail.com>
2020-07-23 02:30:11 +0200

    Upgrade nemerosa.versioning plugin from v2.10.0 to v2.14.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
   plugins {
     id("com.github.ben-manes.versions") version "0.29.0"
-    id("net.nemerosa.versioning") version "2.10.0"
+    id("net.nemerosa.versioning") version "2.14.0"
   }
 }
 
