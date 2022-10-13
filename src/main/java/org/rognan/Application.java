c14cd5729e8b5833fdc936b9da8e3836249bb4be
Thor Rognan <thor.rognan@gmail.com>
2022-10-13 03:40:45 +0200

    Simplify log statement

--- src/main/java/org/rognan/Application.java
+++ src/main/java/org/rognan/Application.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 import java.util.logging.Logger;
 
 import static com.sun.net.httpserver.HttpServer.create;
+import static java.lang.System.getProperty;
 
 public class Application {
   private static final Logger LOG = Logger.getLogger(Application.class.getName());
@@ -23,11 +24,8 @@ public class Application {
 
     server.start();
 
-    final String startupMessage = "Application listening on port %d powered by %s %s"
-      .formatted(port,
-        System.getProperty("java.vendor"),
-        System.getProperty("java.version"));
-
-    LOG.info(startupMessage);
+    LOG.info("Application listening on port %d powered by %s %s".formatted(
+      port, getProperty("java.vendor"), getProperty("java.version")
+    ));
   }
 }
